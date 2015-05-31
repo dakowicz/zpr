c274ec8708aa2bae3ca226569869081ff3048988
tdakowicz <tdakowicz@gmail.com>
2015-05-31 21:16:23 +0200

    Added raw css style for interface

--- app/app.js
+++ app/app.js
@@ -1,12 +1,9 @@
-'use strict';
-
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+angular.module('PokerMain', [
+
+])
+
+.controller('PokerMainCtrl', function($scope){
+      $scope.version = 0.1;
+    })
+;
