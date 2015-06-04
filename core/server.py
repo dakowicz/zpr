de36b932cd322163edf6f8d2589388015949293d
marcin.paszkiewicz <mkpaszkiewicz@gmail.com>
2015-06-04 13:16:13 +0200

    Added handshaking.

--- core/server.py
+++ core/server.py
@@ -4,6 +4,55 @@ import sys
 import json
 import game.player
 from core.table import Table
+import base64
+import hashlib
+
+
+def create_hash(key):
+    hash = hashlib.sha1(key + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11")
+    return base64.b64encode(str(hash.digest()))
+
+
+def recv_data(client, length):
+    data = client.recv(length)
+    if not data: return data
+    return data.decode('utf-8', 'ignore')
+
+
+def send_data(client, data):
+    message = "\x00%s\xFF" % data.encode('utf-8')
+    return client.send(message)
+
+
+def parse_headers(data):
+    headers = {}
+    lines = data.splitlines()
+    for l in lines:
+        parts = l.split(": ", 1)
+        if len(parts) == 2:
+            headers[parts[0]] = parts[1]
+    headers['code'] = lines[len(lines) - 1]
+    return headers
+
+
+def handshake(client):
+    print 'Handshaking...'
+    data = client.recv(1024)
+    headers = parse_headers(data)
+    print 'Got headers:'
+    for k, v in headers.iteritems():
+        print k, ':', v
+
+    digest = create_hash(headers['Sec-WebSocket-Key'])
+    shake = "HTTP/1.1 101 Web Socket Protocol\r\n"
+    shake += "Upgrade: WebSocket\r\n"
+    shake += "Connection: Upgrade\r\n"
+    shake += "Sec-WebSocket-Accept: " + digest + "\r\n"
+    shake += "Sec-WebSocket-Origin: %s\r\n" % (headers['Origin'])
+    shake += "Sec-WebSocket-Location: ws://%s/stuff\r\n" % (headers['Host'])
+    shake += "Sec-WebSocket-Protocol: sample\r\n\r\n"
+
+    return client.send(shake)
 
 
 class Server:
@@ -23,6 +72,7 @@ class Server:
             to close server type 'exit' in standard input
         :return: nothing
         """
+        print create_hash("dGhlIHNhbXBsZSBub25jZQ==")
         self.open_socket()
         input = [self.server, sys.stdin]
         running = True
@@ -33,11 +83,14 @@ class Server:
             for s in input_ready:
                 if s == self.server:
                     player_socket = self.server.accept()
-                    message = json.loads(player_socket[0].recv(Server.package_size))
+                    handshake(player_socket[0])
+                    message = player_socket[0].recv(Server.package_size)
+
+                    #message = json.loads(player_socket[0].recv(Server.package_size))
                     print message
-                    name = message['content']
-                    print 'New player: ' + name
-                    self.get_table().add_player(game.player.Player(name, player_socket))
+                    #name = message['content']
+                    #print 'New player: ' + name
+                    #self.get_table().add_player(game.player.Player(name, player_socket))
 
                 elif s == sys.stdin:
                     command = sys.stdin.readline().rstrip('\n')
@@ -100,4 +153,3 @@ class Server:
                 self.tables.remove(table)
                 i -= 1
                 print 'Empty table removed'
-
