abf5ca77b40eb747a323eb58fa5e0fa1af9a7187
tom3097 <tom3097@gmail.com>
2015-06-06 00:56:33 +0200

    round result of probability calc

--- calculations/CardsCalculations.cpp
+++ calculations/CardsCalculations.cpp
@@ -787,9 +787,9 @@ boost::python::list CalculateProbability(boost::python::list handCards, boost::p
     }
 
     boost::python::list probability;
-    probability.append(floor((double)winCounter/(double)iter + 0.5));
-    probability.append(floor((double)tieCounter/(double)iter + 0.5));
-    probability.append(floor((double)loseCounter/(double)iter + 0.5));
+    probability.append(floor(((double)winCounter/(double)iter)*100 + 0.5));
+    probability.append(floor(((double)tieCounter/(double)iter)*100 + 0.5));
+    probability.append(floor(((double)loseCounter/(double)iter)*100 + 0.5));
 
     return probability;
 }
