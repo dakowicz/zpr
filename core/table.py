421d8a174ed526c3a0c750ba9c7a5a8a64563e69
tom3097 <tom3097@gmail.com>
2015-06-06 03:56:57 +0200

    2000

--- core/table.py
+++ core/table.py
@@ -13,7 +13,7 @@ class Table(threading.Thread):
     """
     PLAYERS_LIMIT = 6
     PLAYERS_MINIMUM = 2
-    TIMEOUT = 500
+    TIMEOUT = 2000
 
     def __init__(self):
         threading.Thread.__init__(self)
